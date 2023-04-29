50b347e3a41e48d398c7aee7570bdb3a33653628
mache102 <bee365go@gmail.com>
2023-04-29 21:42:31 +0800

    update

--- nltk_test.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-
-nltk.download('wordnet')  
-nltk.download('omw-1.4')
-lemmatizer = WordNetLemmatizer()
-  
-print(lemmatizer.lemmatize("shouldn't"))
